0ebb4a0b3936cb68f332d3dd88b1304e089f266d
Roman Ojha <razzroman98@gmail.com>
2022-01-18 10:33:19 +0545

    assetsAudioPlayer tut

--- Tutorial/Topics_Wise/env/lib/Class_07_Media/01_Audio/AssetsAudioPlayerClass.dart
+++ Tutorial/Topics_Wise/env/lib/Class_07_Media/01_Audio/AssetsAudioPlayerClass.dart
@@ -1,12 +1,58 @@
 import 'package:flutter/material.dart';
+import 'dart:io';
+import "package:audioplayers/audioplayers.dart";
 
 // firstly got to the https://pub.dev/
-// after that search for the assets_audio_player
+// after that search for the assets_audio_player or audioplayers
+// https://pub.dev/packages/audioplayers
 // and after that go to the installing tab and then
 // according to the doccument follow that and you are good to go
 
-class AssetsAudioPlayerClass extends StatelessWidget {
-  // AssetsAudioPlayer
+// NOTE: if error came after adding 'audioplayers' package then upgrade the kotlin version
+
+class AssetsAudioPlayerClass extends StatefulWidget {
+  const AssetsAudioPlayerClass({Key? key}) : super(key: key);
+
+  @override
+  _AssetsAudioPlayerClassState createState() => _AssetsAudioPlayerClassState();
+}
+
+class _AssetsAudioPlayerClassState extends State<AssetsAudioPlayerClass> {
+  AudioPlayer audioPlayer = AudioPlayer();
+  late AudioCache audioCache;
+  String audioPath = "music/Music01.mp3";
+
+  @override
+  void initState() {
+    super.initState();
+
+    // if (kIsWeb) {
+    //   return;
+    // }
+    if (Platform.isIOS) {
+      audioCache.fixedPlayer?.notificationService.startHeadlessService();
+    }
+
+    audioCache = AudioCache(fixedPlayer: audioPlayer);
+    audioPlayer.onPlayerStateChanged.listen((event) {});
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    audioPlayer.release();
+    audioPlayer.dispose();
+    audioCache.clearAll();
+  }
+
+  playAudio() async {
+    await audioCache.play(audioPath);
+  }
+
+  pauseAudio() async {
+    await audioPlayer.pause();
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -14,7 +60,14 @@ class AssetsAudioPlayerClass extends StatelessWidget {
         appBar: AppBar(
           title: Text("music Player"),
         ),
-        body: Container(),
+        body: Container(
+          child: IconButton(
+            onPressed: () {
+              playAudio();
+            },
+            icon: Icon(Icons.play_circle),
+          ),
+        ),
       ),
     );
   }
