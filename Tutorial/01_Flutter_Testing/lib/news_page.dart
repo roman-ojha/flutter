23f686516b41ffb85f2bae6eff528fce1b16ebe7
Roman Ojha <razzroman98@gmail.com>
2022-04-20 17:38:20 +0545

    completed Widget test in flutter test

--- Tutorial/01_Flutter_Testing/lib/news_page.dart
+++ Tutorial/01_Flutter_Testing/lib/news_page.dart
@@ -31,7 +31,9 @@ class _NewsPageState extends State<NewsPage> {
         builder: (context, notifier, child) {
           if (notifier.isLoading) {
             return const Center(
-              child: CircularProgressIndicator(),
+              child: CircularProgressIndicator(
+                key: Key('progress-indicator'),
+              ),
             );
           }
           return ListView.builder(
--- Tutorial/01_Flutter_Testing/test/news_page_test.dart
+++ Tutorial/01_Flutter_Testing/test/news_page_test.dart
@@ -30,6 +30,14 @@ void main() {
         .thenAnswer((_) async => articlesFromServices);
   }
 
+  void arrangeNewsServicesReturns3ArticlesAfter2SecondWait() {
+    // rather then calling this 'when' function ever time we will create the new function
+    when(() => mockNewsServices.getArticles()).thenAnswer((_) async {
+      await Future.delayed(const Duration(seconds: 2));
+      return articlesFromServices;
+    });
+  }
+
   Widget createWidgetUnderTest() {
     return MaterialApp(
       title: 'News App',
@@ -65,5 +73,39 @@ void main() {
 
     expect(find.text("News"), findsOneWidget);
     // here we would expect to see one text widget which will call as 'News'
+  });
+
+  testWidgets("loading indicator is displayed while waiting for articles",
+      (WidgetTester tester) async {
+    // now what we can test is: if while article are being loaded from the server that the loading or the circular progress indicator is being shown
+    // so here we are waiting for the article to come from let's say server
+    arrangeNewsServicesReturns3ArticlesAfter2SecondWait();
+    await tester.pumpWidget(createWidgetUnderTest());
+
+    // expect(find.byType(CircularProgressIndicator), findsOneWidget);
+    // nowhere we are finding The widget and expect to get it
+    // but This is not enough because this is not static ui like the test will happen so quickly and those widget that where build after that test example "Consumer<NewsChangeNotifier>" tester will not found any widget that we have specify
+
+    await tester.pump(const Duration(milliseconds: 500));
+    // now here pump forces to rebuild widget for a given time and we want to get the 'CircularProgressIndicator' which will come much before the article data so we will going to rebuild widget for '400ms'
+    expect(find.byType(CircularProgressIndicator), findsOneWidget);
+    // and other way to find widget is by 'find.ByKey()'
+    expect(find.byKey(const Key('progress-indicator')), findsOneWidget);
+
+    await tester.pumpAndSettle();
+    // https://youtu.be/Ghqry5dtgH4?t=1724
+    // 'pump' and 'settle' wait until there no more rebuild happening
+  });
+
+  testWidgets("articles should displayed", (WidgetTester tester) async {
+    arrangeNewsServicesReturns3Articles();
+
+    await tester.pumpWidget(createWidgetUnderTest());
+    await tester.pump();
+    for (final article in articlesFromServices) {
+      // now here we will check every article that we will get
+      expect(find.text(article.title), findsOneWidget);
+      expect(find.text(article.content), findsOneWidget);
+    }
   });
 }
