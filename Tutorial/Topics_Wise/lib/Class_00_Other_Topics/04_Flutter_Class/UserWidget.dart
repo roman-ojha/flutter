e81d5a88a85c4fb36840595e834f707ec7c84156
Roman-Ojha <razzroman98@gmail.com>
2022-07-19 10:32:30 +0545

    docs: flutter class create UserWidget

--- Tutorial/Topics_Wise/lib/Class_00_Other_Topics/04_Flutter_Class/UserWidget.dart
+++ Tutorial/Topics_Wise/lib/Class_00_Other_Topics/04_Flutter_Class/UserWidget.dart
@@ -1,14 +1,27 @@
-// import 'package:flutter/cupertino.dart';
-// import 'package:flutter/src/foundation/key.dart';
-// import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/material.dart';
 
-// class ShowUser extends StatelessWidget {
-//   const ShowUser({Key? key}) : super(key: key);
+class ShowUser extends StatelessWidget {
+  final String firstName;
+  // final keyword because this value for tha particular instance will be final and will not going to change
+  final String lastName;
+  final int userID;
 
-//   @override
-//   Widget build(BuildContext context) {
-//     return Container(
-      
-//     ) ;
-//   }
-// }
+  ShowUser(
+      {Key? key,
+      required this.firstName,
+      required this.lastName,
+      required this.userID})
+      : super(key: key);
+  // Widget based constructor to get the data
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: ListView(children: [
+        Text("ID: ${userID.toString()}"),
+        Text("FirstName: $firstName"),
+        Text("LastName: $lastName")
+      ]),
+    );
+  }
+}
