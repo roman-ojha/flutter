9355e6776c5dc1fc32f308e43a26ac627896381e
Roman-Ojha <razzroman98@gmail.com>
2023-06-15 17:56:46 +0545

    feat(practice): created multidimensional scrollable items

--- Practice/02/lib/screen/second.dart
+++ Practice/02/lib/screen/second.dart
@@ -7,10 +7,11 @@ class SecondPage extends StatelessWidget {
   final Random random = Random();
   SecondPage({super.key, required this.title});
 
-  Widget getContainer() {
+  Widget getContainer(int verticalIndex, int horizontalIndex) {
     return Container(
       width: 100.0,
       // height: 100.0,
+      margin: const EdgeInsets.only(top: 30.0, left: 20.0),
       decoration: BoxDecoration(
         color: Color.fromRGBO(
           random.nextInt(256),
@@ -18,30 +19,39 @@ class SecondPage extends StatelessWidget {
           random.nextInt(256),
           1,
         ),
+        borderRadius: const BorderRadius.all(Radius.circular(10.0)),
       ),
+      child: Center(child: Text("Item [$verticalIndex] [$horizontalIndex]")),
     );
   }
 
   @override
   Widget build(BuildContext context) {
-    ScrollController _scrollController = ScrollController();
     return Scaffold(
       appBar: AppBar(
         title: Text(title),
       ),
       drawer: const AppDrawer(),
-      body: Container(
-        height: 200,
-        child: Scrollbar(
-          thumbVisibility: true,
-          controller: _scrollController,
-          child: ListView.builder(
-            scrollDirection: Axis.horizontal,
-            itemCount: 50,
-            itemBuilder: (BuildContext context, int index) => getContainer(),
-            controller: _scrollController,
-          ),
-        ),
+      body: ListView.builder(
+        itemCount: 50,
+        controller: PrimaryScrollController.of(context),
+        itemBuilder: (BuildContext context, int verticalIndex) {
+          ScrollController scrollController = ScrollController();
+          return SizedBox(
+            height: 200, // Set the desired height for each vertical item
+            child: Scrollbar(
+              thumbVisibility: true,
+              controller: scrollController,
+              child: ListView.builder(
+                scrollDirection: Axis.horizontal,
+                itemCount: 50,
+                controller: scrollController,
+                itemBuilder: (BuildContext context, int horizontalIndex) =>
+                    getContainer(verticalIndex, horizontalIndex),
+              ),
+            ),
+          );
+        },
       ),
     );
   }
